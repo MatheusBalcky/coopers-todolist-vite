8c517fdd62ab819aaac78113acf787025112b33e
Matheus Melo <matheuscabal821@gmail.com>
2023-01-31 20:32:50 -0300

    📦 NEW: Routes added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage/HomePage';
+import SignupPage from './pages/SignupPage/SignupPage';
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={'*' || '/'} element={<HomePage />} />
+        <Route path={'/signup'} element={<SignupPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;

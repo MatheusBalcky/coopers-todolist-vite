7aff82b0cce28d9f82e7a34f4aa917a122d82c3d
Matheus Melo <matheuscabal821@gmail.com>
2023-02-01 15:35:36 -0300

    👌 IMPROVE: From code refact

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage/HomePage';
+import HomePage from './pages/HomePage';
 import SignupPage from './pages/SignupPage/SignupPage';
 
 function App() {

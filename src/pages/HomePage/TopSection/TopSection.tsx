f5c0386b7516775bd3bfff6195ced2765b31736e
Matheus Melo <matheuscabal821@gmail.com>
2023-02-04 16:36:14 -0300

    👌 IMPROVE: Code refactoring

---
+++ src/pages/HomePage/TopSection/TopSection.tsx
@@ -0,0 +1,171 @@
+import styled from 'styled-components';
+import BgAsideHeaderSection from '../../../assets/bg-aside-headersection.svg';
+import RoomPhoto from '../../../assets/room-photo.png';
+import LogoCoopers from '../../../assets/logo-coopers.svg';
+import { IoEnter } from 'react-icons/io5';
+
+export default function TopSection() {
+  return (
+    <TopSectionContainer>
+      <BackgroundAside dir={BgAsideHeaderSection}>
+        <img src={RoomPhoto} alt="Imagem de uma sala organizada e bonita" />
+      </BackgroundAside>
+      <Header>
+        <h1 id="logo-header">
+          <img id="logo-coopers-svg" src={LogoCoopers} alt="Logo do website que se chama COOPERS" />
+        </h1>
+
+        <IoEnter className="IoEnter" />
+        <button id="login-button">entrar</button>
+      </Header>
+
+      <SectionMainArticle>
+        <Article>
+          <p id="big-paragraph">
+            <b>Organize</b> <br />
+            your daily jobs
+          </p>
+
+          <p id="small-paragraph">The only way to get things done</p>
+
+          <button id="goto-todo-button">Go to To-do list</button>
+        </Article>
+      </SectionMainArticle>
+    </TopSectionContainer>
+  );
+}
+
+// & CSS STYLED-COMPONENTS...
+
+const TopSectionContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  height: 750px;
+  position: relative;
+`;
+
+const BackgroundAside = styled.div`
+  position: absolute;
+  width: 50%;
+  height: 100%;
+  right: 0px;
+  top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-image: url(${(props) => props.dir});
+  background-position: right;
+  background-size: contain;
+  background-repeat: no-repeat;
+
+  img {
+    animation: float 3s ease-in-out infinite;
+    width: 50%;
+  }
+
+  @keyframes float {
+    0% {
+      box-shadow: 0 5px 15px 0px rgba(0, 0, 0, 0.6);
+      transform: translatey(0px);
+    }
+    50% {
+      box-shadow: 0 15px 15px 0px rgba(0, 0, 0, 0.2);
+      transform: translatey(-5px);
+    }
+    100% {
+      box-shadow: 0 5px 15px 0px rgba(0, 0, 0, 0.6);
+      transform: translatey(0px);
+    }
+  }
+
+  @media (max-width: 670px) {
+    display: none;
+  }
+`;
+
+const Header = styled.header`
+  z-index: 1;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  box-sizing: border-box;
+  padding: 15px 35px;
+
+  .IoEnter {
+    font-size: 2em;
+  }
+
+  #login-button {
+    display: none;
+    padding: 5px 35px;
+    background-color: black;
+    color: white;
+    transition: 0.2s all;
+    &:hover {
+      filter: opacity(70%);
+    }
+  }
+
+  #logo-coopers-svg {
+    width: 200px;
+  }
+
+  @media (min-width: 500px) {
+    .IoEnter {
+      display: none;
+    }
+    #login-button {
+      display: inherit;
+    }
+  }
+`;
+
+const SectionMainArticle = styled.article`
+  display: flex;
+  height: 650px;
+`;
+
+const Article = styled.article`
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 30px;
+  width: 60%;
+  padding: 25px 35px;
+
+  button {
+    box-sizing: border-box;
+    padding: 10px 25px;
+    max-width: 200px;
+    border-radius: 10px;
+    background-color: #4ac959;
+    color: white;
+    font-weight: 600;
+    transition: 0.2s all;
+    &:hover {
+      filter: opacity(70%);
+    }
+  }
+
+  #big-paragraph {
+    font-size: 3.5em;
+    color: #4ac959;
+    b {
+      color: black;
+    }
+  }
+
+  #small-paragraph {
+    font-weight: 600;
+  }
+
+  @media (max-width: 670px) {
+    align-items: center;
+    width: 100%;
+    #big-paragraph,
+    #small-paragraph {
+      text-align: center;
+    }
+  }
+`;
